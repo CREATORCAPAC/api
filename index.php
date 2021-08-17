9c69a90fad40de1e56384c18f4f916d30ebc8b3c
Creator Capac <julioayastagrande@gmail.com>
2021-08-17 14:03:00 -0700

    test

--- index.php
+++ index.php
@@ -142,10 +142,10 @@ $valid_url_regex = '/.*/';
 
 // ############################################################################
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$url = str_replace(['34.72.3.233/api','localhost/api','factoringonline.pe/api'], "factoringonline.net.pe/HAWKEYE", $actual_link);
+$url = str_replace(['34.72.3.233/api','localhost/api','factoringonline.pe/api'], "localhost/HAWKEYE", $actual_link);
 $url = str_replace('/HAWKEYE/?','/HAWKEYE/',$url);    
-/*echo $url;
-exit();*/
+echo $url;
+exit();
 //$url = $_GET['url'];
 
 if ( !$url ) {
